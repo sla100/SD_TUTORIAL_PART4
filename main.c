2bdacab6a7aacd46fe4ad280d035132476c791be
Slawomir Osoba <slawomir.osoba@gmail.com>
2023-05-04 21:44:31 +0200

    Baud 1M

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include "sdcard.h"
 #include "sdprint.h"
 
-#define BAUD_RATE 57600
+#define BAUD_RATE 1000000
 
 int main(void)
 {
@@ -49,30 +49,6 @@ int main(void)
             SD_printDataErrToken(token);
         }
 
-        // update address to 0x00000100
-        addr = 0x00000100;
-
-        // fill buffer with 0x55
-        for(uint16_t i = 0; i < 512; i++) buf[i] = 0x55;
-
-        UART_pputs("Writing 0x55 to sector: 0x");
-        UART_puthex8((uint8_t)(addr >> 24));
-        UART_puthex8((uint8_t)(addr >> 16));
-        UART_puthex8((uint8_t)(addr >> 8));
-        UART_puthex8((uint8_t)addr);
-
-        // write data to sector
-        res[0] = SD_writeSingleBlock(addr, buf, &token);
-
-        UART_pputs("\r\nResponse:\r\n");
-        SD_printR1(res[0]);
-
-        // if no errors writing
-        if(res[0] == 0x00)
-        {
-            if(token == SD_DATA_ACCEPTED)
-                UART_pputs("Write successful\r\n");
-        }
     }
 
     while(1) ;
